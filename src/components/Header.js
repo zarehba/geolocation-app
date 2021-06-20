332a657f77cce0cb430905d7ba9376b2ddbaed6c
zarehba <zarehba@protonmail.com>
2021-06-20 12:37:31 +0200

    add styled Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import GlobeIcon from '@material-ui/icons/Public';
+
+const useStyles = makeStyles(() => ({
+  title: { marginLeft: '16px' },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <AppBar position='absolute'>
       <Toolbar>
+        <GlobeIcon />
+        <Typography
+          component='h1'
+          variant='h6'
+          noWrap
+          className={classes.title}
+        >
+          Look up location of an IP/URL
+        </Typography>
       </Toolbar>
     </AppBar>
   );
