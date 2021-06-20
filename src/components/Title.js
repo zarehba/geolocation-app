3dcdf6f5dddd5a4c0d53238fac7ae350bec00c4c
zarehba <zarehba@protonmail.com>
2021-06-20 13:28:15 +0200

    add styling for Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Typography from '@material-ui/core/Typography';
@@ -8,10 +9,16 @@ const Title = (props) => {
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Typography component='h2'>
-      <Box p={isSmall ? 1 : 2}>{props.children}</Box>
+    <Typography component='h2' variant='h6' color='primary'>
+      <Box fontWeight='fontWeightBold' p={isSmall ? 1 : 2}>
+        {props.children}
+      </Box>
     </Typography>
   );
+};
+
+Title.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Title;
